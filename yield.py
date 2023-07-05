5c5d60d01a3e572079cf20df438a17043ae79bdc
YasiruNilupul <wickramageyn.22@UOM.lk>
2023-07-05 17:46:15 +0530

    Update yield.py

--- yield.py
+++ yield.py
@@ -1,4 +1,5 @@
 num = int(input())
+
 def show_decimal(num):
     decimal_list = []
     for i in range(1,num + 1):
@@ -7,22 +8,22 @@ def show_decimal(num):
     #return decimal_list
 
 def show_octal(num):
-    octal_list = []
+    #octal_list = []
     octal_num = []
     while True:
-        if num % 2 == 0:
-            octal_num.insert(0,1)
+        if num ==0:
+            break
+        elif num % 2 == 0:
+            octal_num.insert(0,0)
         elif num % 2 == 1:
             octal_num.insert(0,1)
-        elif num ==0:
-            break
+        
         num = num // 2
-    return octal_num
+    return [str(x) for x in octal_num]
+# print("".join(show_octal(num)))
+
 number = show_decimal(num)
+
 for i in number:
     binary = show_octal(i)
-    print(binary)
-        
-    
-  
-
+    print("".join(binary))
